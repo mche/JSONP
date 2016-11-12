0eef543b3dcae0e7f4976033b7fd6df99089faa9
Михаил Че (Mikhail Che) <mche@users.noreply.github.com>
2016-11-12 16:42:06 +0600

    Update jsonp.js

--- jsonp.js
+++ jsonp.js
@@ -25,45 +25,47 @@ var JSONP = (function(){
   
   self._cleaner = function (sender) {
     return function () {
-      document.body.removeChild(sender.script);
+      if ( sender === undefined ) return;
       window.clearTimeout(sender.timeout_trigger);
-      delete self._R[sender.callback_name];
+      document.body.removeChild(sender.script);
+      delete self._R[sender.callback_name];// харакири
+      sender = undefined;
     };
     
   };
   
   self.send = function(url, options, onSent, onSuccess, onError, onTimeout) {
     var options = options || {},
-      callback_name = '_cb_' + Math.round(1e10 * Math.random()) //options.callbackName || 'callback',
+      callback_name = '_cb_' + Math.round(1e10 * Math.random())
         .toString(36) + '_' + (self.COUNTER++).toString(36),
-      onSent = onSent || options.onSent,
-      onSuccess = onSuccess || options.onSuccess,
-      onError = onError || options.onError,
-      onTimeout = onTimeout || options.onTimeout,
       timeout = options.timeout || 15,
       async = options.async === undefined ? true : options.async,
-      _url = url + (~url.indexOf('?') ? '&' : '?') + 'callback=JSONP._R.' + callback_name+'.cb',
       cbOk = false,
       debug = options.debug || false;
-      
-    
-    var sender = self._R[callback_name] = {};
-    sender.callback_name = callback_name;
+
+    var sender = self._R[callback_name] = {
+      callback_name: callback_name,
+      onSent: onSent || options.onSent,
+      onSuccess: onSuccess || options.onSuccess,
+      onError: onError || options.onError,
+      onTimeout: onTimeout || options.onTimeout,
+      url: url + (~url.indexOf('?') ? '&' : '?') + 'callback=JSONP._R.' + callback_name+'.cb'
+    };
     
     var clean = self._cleaner(sender);
 
     sender.timeout_trigger = window.setTimeout(function(){
-      debug && console.log('Timeout JSONP => ' +_url);
-      clean(); // можно очистить реестр
-      onTimeout && onTimeout(url);
+      debug && console.log('Timeout JSONP => ' +sender.url);
+      clean(); // можно очистить
+      sender.onTimeout && sender.onTimeout(url);
     }, timeout * 1000);
 
     
     sender.cb = function (data) {
       cbOk = true;  // обработчик вызвался, указать что всё ок
-      debug && console.log('Success JSONP => ' +_url);
+      debug && console.log('Success JSONP => ' + sender.url);
       clean(); // можно очистить
-      onSuccess && onSuccess(data, url);
+      sender.onSuccess && sender.onSuccess(data, url);
     };
     
     /*
@@ -72,9 +74,9 @@ var JSONP = (function(){
     */
     sender.checker = function () {
       if (cbOk) return; // сработал обработчик? нет - вызвать onError
-      debug && console.log('Error JSONP => ' +_url);
-      clean();
-      onError && onError(url);
+      debug && console.log('Error JSONP => ' +sender.url);
+      clean(); // можно очистить
+      sender.onError && sender.onError(url);
     };
 
     sender.script = document.createElement('script');
@@ -96,11 +98,11 @@ var JSONP = (function(){
   
     sender.script.type = 'text/javascript';
     sender.script.async = async;
-    sender.script.src = _url;
+    sender.script.src = sender.url;
 
     //~ document.getElementsByTagName('head')[0].appendChild(script);
     document.body.appendChild(sender.script);
-    debug && console.log("JSONP sent: "+_url);
+    debug && console.log("JSONP sent: "+sender.url);
     onSent && onSent(url);
   };
 
